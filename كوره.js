6ce8affe4664232251b3d5ab027cff7ea7b58d92
itachidrox <161158712+itachidrox@users.noreply.github.com>
2024-03-01 13:44:24 +0100

    Create كوره.js

---
+++ كوره.js
@@ -0,0 +1,321 @@
+{
+        "img": "https://telegra.ph/file/052910998329d7b258bb6.jpg ",
+        "name": "ديبالا "
+    },
+    {
+        "img": "https://telegra.ph/file/ca16a2c7577b229600dfd.jpg ",
+        "name": "فليكس "
+    },
+    {
+        "img": "https://telegra.ph/file/d97dca677400d1628e99c.jpg ",
+        "name": "سواريز "
+    },
+    {
+        "img": "https://telegra.ph/file/c6c9310e1562f9fffe6d5.jpg ",
+        "name": "سون "
+    },
+    {
+        "img": "https://telegra.ph/file/5d2a19ac9d45735d70e1d.jpg ",
+        "name": "محمد صلاح "
+    },
+    {
+        "img": "https://telegra.ph/file/b97908e9f9e126158d7a5.jpg ",
+        "name": "ليفاندوسكي "
+    },
+    {
+        "img": "https://telegra.ph/file/3a8563501ed73dce30c8f.jpg ",
+        "name": "راموس "
+    },
+    {
+        "img": "https://telegra.ph/file/ef1fea962ae789e5ad2e9.jpg ",
+        "name": "مبابي "
+    },
+    {
+        "img": "https://telegra.ph/file/2c5ee9bc0b652c004f82d.jpg ",
+        "name": "نيمار "
+    },
+    {
+        "img": "https://telegra.ph/file/ae82809950a94923ecf19.jpg ",
+        "name": "بيدري "
+    },
+{
+        "img": "https://telegra.ph/file/c39174c03ed2659752ca4.jpg ",
+        "name": "بيلينغهام "
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+{
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+    {
+        "img": "",
+        "name": ""
+    },
+]
