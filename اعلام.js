efdd0818db4c48ff1fc58320bb73774b3a32a7fc
itachidrox <161158712+itachidrox@users.noreply.github.com>
2024-03-06 15:57:50 +0100

    اعلام.js

---
+++ اعلام.js
@@ -0,0 +1,205 @@
+[
+    {
+        "img": "https://flagpedia.net/data/flags/w702/af.png",
+        "name": "افغانستان"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ax.png",
+        "name": "جزر الاند"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/al.png",
+        "name": "البانيا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/dz.png",
+        "name": "الجزائر"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/as.png",
+        "name": "ساموا الأمريكية"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ad.png",
+        "name": "اندورا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ao.png",
+        "name": "انغولا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bs.png",
+        "name": "جزر البهاما"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bh.png",
+        "name": "البحرين"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bd.png",
+        "name": "بنغلاديش"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bb.png",
+        "name": "بربادوس"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/by.png",
+        "name": "بيلاروسيا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/be.png",
+        "name": "بلجيكا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bz.png",
+        "name": "بليز"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bj.png",
+        "name": "بنين"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bm.png",
+        "name": "برمودا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bt.png",
+        "name": "بوتان"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bo.png",
+        "name": "بوليفيا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ba.png",
+        "name": "البوسنة والهرسك"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bw.png",
+        "name": "بوتسوانا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bv.png",
+        "name": "جزيرة بوفيه"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/br.png",
+        "name": "البرازيل"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/io.png",
+        "name": "إقليم المحيط البريطاني الهندي"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bn.png",
+        "name": "بروناي"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bg.png",
+        "name": "بلغاريا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bf.png",
+        "name": "بوركينا فاسو"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bi.png",
+        "name": "بوروندي"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/kh.png",
+        "name": "كمبوديا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cm.png",
+        "name": "الكاميرون"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ca.png",
+        "name": "كندا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cv.png",
+        "name": "الرأس الأخضر"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/bq.png",
+        "name": "هولندا الكاريبية"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ky.png",
+        "name": "جزر كايمان"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cf.png",
+        "name": "جمهورية افريقيا الوسطى"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/td.png",
+        "name": "تشاد"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cl.png",
+        "name": "شيلي"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cn.png",
+        "name": "الصين"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cx.png",
+        "name": "جزيرة عيد الميلاد"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cc.png",
+        "name": "جزر كوكوس"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/co.png",
+        "name": "كولومبيا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/km.png",
+        "name": "جزر القمر"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cg.png",
+        "name": "جمهورية الكونغو"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cd.png",
+        "name": "جمهورية الكونغو الديمقراطية"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ck.png",
+        "name": "جزر كوك"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cr.png",
+        "name": "كوستا ريكا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/ci.png",
+        "name": "ساحل العاج"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/hr.png",
+        "name": "كرواتيا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cu.png",
+        "name": "كوبا"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cw.png",
+        "name": "كوراكاو"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cy.png",
+        "name": "قبرص"
+    },
+    {
+        "img": "https://flagpedia.net/data/flags/w702/cz.png",
+        "name": "التشيك"
+    },
